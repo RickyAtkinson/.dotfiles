98678ba463666b85ff35eb6e3298adf0135f8bd9
RickyAtkinson <ricky@kinson.digital>
2024-02-26 21:37:09 +0000

    Fix neovim WSL check

--- config/neovim/init.lua
+++ config/neovim/init.lua
@@ -4,7 +4,7 @@ _G.OS = uname.sysname
 _G.IS_MAC = OS == "Darwin"
 _G.IS_LINUX = OS == "Linux"
 _G.IS_WINDOWS = OS:find("Windows") and true or false
-_G.IS_WSL = IS_LINUX and uname.release:find("Microsoft") and true or false
+_G.IS_WSL = IS_LINUX and uname.release:lower():find("microsoft") and true or false
 
 -- Bootstrap lazy.nvim, LazyVim and custom plugins
 require("config.lazy")
