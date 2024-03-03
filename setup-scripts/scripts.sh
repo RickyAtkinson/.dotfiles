b573fc86a47edcece5ae4655f52c326c29b14703
RickyAtkinson <ricky@kinson.digital>
2024-03-03 03:59:58 +0000

    Remove redundant code from scripts setup

--- setup-scripts/scripts.sh
+++ setup-scripts/scripts.sh
@@ -2,7 +2,7 @@
 #
 # Installs scripts
 
-source $HOME/.dotfiles/config/scripts/lib/utils.sh
+source "$HOME/.dotfiles/config/scripts/lib/utils.sh"
 
 echo_info "Installing scripts..."
 
@@ -10,18 +10,10 @@ dotfile_dir="$HOME/.dotfiles/config/scripts"
 config_dir="$HOME/.local/scripts"
 
 # Cleanup to avoid broken links hanging around
-rm -r $config_dir/* 2>/dev/null
-
-[ ! -d $config_dir/lib ] && mkdir -p $config_dir/lib
+rm -r "$config_dir/*" 2>/dev/null
 
 for dotfile in "$dotfile_dir"/*.sh; do
   link_file="$config_dir${dotfile#"$dotfile_dir"}"
   link_file="${link_file%".sh"}"
-  ln -sf $dotfile $link_file
-  chmod +x "$link_file"
-done
-
-for dotfile in "$dotfile_dir"/lib/*; do
-  link_file="$config_dir${dotfile#"$dotfile_dir"}"
-  ln -sf $dotfile $link_file
+  ln -sf "$dotfile" "$link_file"
 done
