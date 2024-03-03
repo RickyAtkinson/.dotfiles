509c4b2614517590b9ff9bdd498ccb71141679b2
RickyAtkinson <ricky@kinson.digital>
2024-03-03 13:01:21 +0000

    Remove autocmd that cause errors on save

--- config/neovim/lua/config/autocmds.lua
+++ config/neovim/lua/config/autocmds.lua
@@ -34,12 +34,13 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
 })
 
 -- Create directories when needed, when saving a file
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = vim.api.nvim_create_augroup("better_backup", { clear = true }),
-  callback = function(event)
-    local file = vim.loop.fs_realpath(event.match) or event.match
-    local backup = vim.fn.fnamemodify(file, ":p:~:h")
-    backup = backup:gsub("[/\\]", "%%")
-    vim.go.backupext = backup
-  end,
-})
+-- TODO: This seems to cause an error when the file path contains square brackets
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   group = vim.api.nvim_create_augroup("better_backup", { clear = true }),
+--   callback = function(event)
+--     local file = vim.loop.fs_realpath(event.match) or event.match
+--     local backup = vim.fn.fnamemodify(file, ":p:~:h")
+--     backup = backup:gsub("[/\\]", "%%")
+--     vim.go.backupext = backup
+--   end,
+-- })
